019b30a5ffa5af1c286bc92690458325f8c0cdd9
MatiasGonzalez <matiaspch@gmail.com>
2021-05-29 21:09:11 -0300

    Request errores en castellano + function login

--- BackEnd/donARAPI/app/Http/Controllers/UsersController.php
+++ BackEnd/donARAPI/app/Http/Controllers/UsersController.php
@@ -8,13 +8,18 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    public function register(RegisterRequest $request)
+    public function register(RegisterRequest $request): \Illuminate\Http\JsonResponse
     {
         // dd($request->all());
         $request->merge(['password' => bcrypt($request->input('password'))]);
 
         $User = User::create($request->only(['email','password']));
 
-        return response()->json(['response'=>$User],200);
+        return response()->json(['response'=>$User]);
+    }
+
+    public function login()
+    {
+        
     }
 }
